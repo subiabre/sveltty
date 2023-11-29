b3d419af9de097193f6af31adca20b3b768e96ed
Daniel Subiabre <daniel.subiabre@platoniq.net>
2023-11-29 18:10:25 +0100

    Command clear also clears url

--- src/lib/Command/CommandClear.svelte
+++ src/lib/Command/CommandClear.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import { stdout } from "$lib/stdout";
     import { onMount } from "svelte";
 
     onMount(() => {
         stdout.clear();
+
+        goto('/');
     });
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+    import { afterNavigate } from "$app/navigation";
     import MessageWelcome from "$lib/Message/MessageWelcome.svelte";
+    import { stdin } from "$lib/stdin";
     import { stdout } from "$lib/stdout";
-    import { onMount } from "svelte";
 
-    onMount(() => stdout.write(MessageWelcome));
+    afterNavigate(() => {
+        if ($stdin.input.length < 2) {
+            stdout.write(MessageWelcome);
+        }
+    });
 </script>
