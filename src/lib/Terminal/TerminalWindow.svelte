e65ac7c14e62fc7be75101813d99f5870f1ca8bf
Daniel Subiabre <subiabrewd@gmail.com>
2024-04-13 21:38:35 +0200

    Manage terminal window in dedicated component

---
+++ src/lib/Terminal/TerminalWindow.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let div: HTMLDivElement;
+    let divScroll: number = 0;
+
+    export function scroll() {
+        if (typeof div === "undefined") return;
+
+        setTimeout(() => {
+            div.scroll({
+                top: divScroll,
+                behavior: "smooth",
+            });
+        }, 100);
+
+        setTimeout(() => {
+            divScroll = div.scrollHeight;
+        }, 200);
+    }
+</script>
+
+<div bind:this={div}>
+    <slot />
+</div>
+
+<style>
+    div {
+        height: calc(100% - 2em);
+        max-height: calc(100dvh - 2em);
+
+        padding: 1em;
+
+        overflow-y: scroll;
+    }
+</style>
