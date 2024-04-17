dd723ec54c5057c840a2ac36332844966210e623
Daniel Subiabre <subiabrewd@gmail.com>
2024-04-17 20:13:19 +0200

    Start layout rewrite with simplification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,101 +1,25 @@
 <script lang="ts">
     import "../app.css";
-    import CommandPrompt from "$lib/Command/CommandPrompt.svelte";
+    import { shell } from "$lib/shell";
+    import { stdout } from "$lib/stdout";
     import TerminalInput from "$lib/Terminal/TerminalInput.svelte";
     import TerminalOutput from "$lib/Terminal/TerminalOutput.svelte";
+    import TerminalWindow from "$lib/Terminal/TerminalWindow.svelte";
     import TerminalPrompt from "$lib/Terminal/TerminalPrompt.svelte";
-    import { shell } from "$lib/shell";
-    import { stdin } from "$lib/stdin";
-    import { stdout } from "$lib/stdout";
+    import CommandPrompt from "$lib/Command/CommandPrompt.svelte";
 
-    let main: HTMLElement;
-    let scroll: number = 0;
+    function handleSubmit(e: CustomEvent) {
+        const command = shell.exec(e.detail.args[0]);
 
-    let input: TerminalInput;
-    let output: TerminalOutput;
-
-    async function handleInput(
-        event: CustomEvent<{ value: string; keyboard: KeyboardEvent }>,
-    ) {
-        // Autocomplete
-        if (
-            event.detail.keyboard.key === "Tab" &&
-            event.detail.keyboard.shiftKey === false
-        ) {
-            event.detail.keyboard.preventDefault();
-
-            let args = event.detail.value.trimEnd()?.split(" ");
-
-            const auto = await shell.auto(args);
-            input.write(auto.join(" "));
-            input.focus();
-        }
-
-        // Submit
-        if (event.detail.keyboard.key === "Enter") {
-            event.detail.keyboard.preventDefault();
-
-            const args = $stdin.input[$stdin.index]
-                .split(" ")
-                .filter((arg) => arg);
-
-            stdin.send();
-
-            stdout.write(CommandPrompt, { args });
-            stdout.write(shell.exec(args[0]), { args });
-        }
-
-        // Previous input
-        if (event.detail.keyboard.key === "ArrowUp") {
-            stdin.prev();
-            input.focus();
-        }
-
-        // Next input
-        if (event.detail.keyboard.key === "ArrowDown") {
-            stdin.next();
-            input.focus();
-        }
+        stdout.write(CommandPrompt, { args: e.detail.args });
+        stdout.write(command, { args: e.detail.args });
     }
-
-    stdout.subscribe(() => {
-        if (typeof main !== "object") return;
-
-        setTimeout(() => {
-            main.scroll({
-                top: scroll,
-                behavior: "smooth",
-            });
-        }, 100);
-
-        setTimeout(() => {
-            scroll = main.scrollHeight;
-        }, 200);
-    });
 </script>
 
-<main bind:this={main}>
-    <div>
-        <slot />
-        <TerminalOutput bind:this={output} values={$stdout} />
-        <TerminalPrompt>
-            <TerminalInput
-                bind:this={input}
-                bind:value={$stdin.input[$stdin.index]}
-                on:input={handleInput}
-            />
-        </TerminalPrompt>
-    </div>
-</main>
-
-<style>
-    main {
-        height: 100%;
-
-        overflow-y: scroll;
-    }
-
-    div {
-        max-width: 75ch;
-    }
-</style>
+<TerminalWindow>
+    <slot />
+    <TerminalOutput bind:values={$stdout} />
+    <TerminalPrompt>
+        <TerminalInput on:submit={handleSubmit} />
+    </TerminalPrompt>
+</TerminalWindow>
